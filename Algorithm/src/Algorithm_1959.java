acc8da53dcf2899c218d79e061a320391a0e29c6
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-03-21 16:34:30 +0900

    SWEA 1959 - Code fix

--- Algorithm/src/Algorithm_1959.java
+++ Algorithm/src/Algorithm_1959.java
@@ -9,36 +9,44 @@ public class Algorithm_1959 {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         
-        
         for(int test_case = 1; test_case <= T; test_case++)
         {
             int N = sc.nextInt();   // 배열 A의 원소 수
             int M = sc.nextInt();   // 배열 B의 원소 수
             int[] A = new int[N];
             int[] B = new int[M];
-            int[] tmp = new int[Math.max(N-M+1, M-N+1)];
-            int ans = 0;
+//            int[] tmp = new int[Math.max(N-M+1, M-N+1)];
+            int ans;
              
             for(int i=0; i<N; ++i)
                 A[i] = sc.nextInt();
             for(int i=0; i<M; ++i)
                 B[i] = sc.nextInt();
              
-            for(int i =0; i<tmp.length; ++i) {
-                if(N>=M)
-                    for(int j=0; j<M; ++j) {
-                        tmp[i] += ( A[i+j]*B[j] );
-                    }
-                else
-                    for(int j=0; j<N; ++j) {
-                        tmp[i] += ( A[j]*B[i+j] );
-                    }
-            }
-            Arrays.sort(tmp);
-            ans = tmp[tmp.length-1];
+            if(N>=M) 	ans = calculate(A, B);
+            else		ans = calculate(B, A);
+            
             System.out.println("#"+test_case+" "+ans);
         }
         sc.close();
+	}
+	
+	static int calculate(int[] a, int[] b) {
+		int[] tmp = new int[ a.length - b.length +1 ];
+//		int result = Integer.MIN_VALUE;
+		
+		for(int i =0; i<=a.length - b.length; ++i) {
+			int sum = 0;
+			for(int j=0; j<b.length; ++j)
+//				sum += ( a[i+j] * b[j] );
+				tmp[i] += ( a[i+j] * b[j] );
+//			result = Math.max(sum, result);
+		}
+		
+        Arrays.sort(tmp);	// 오름차순 정렬
+        
+        return tmp[tmp.length-1];	// 마지막 원소
+//		return result;
 	}
 
 }
