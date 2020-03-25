cd2fc696520f814659e7f6f1018411f3cbca58c2
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-03-25 12:10:47 +0900

    Codility 02 - Cyclic Rotation code fix

--- Algorithm/src/Codility_02.java
+++ Algorithm/src/Codility_02.java
@@ -1,15 +1,20 @@
 
 public class Codility_02 {
-	public static void main(String[] args) throws Exception {
-		int[] A = {999, 500, 0, -1000, 76};
+	public static void main(String[] args) {
+//		int[] A = {999, 500, 0, -1000, 76};
+		int[] A = {};
 		int[] B = new int[A.length];
 		
 		System.out.print("Before: ");
 		for(int i : A) 
 			System.out.print(i+" ");
-		System.out.println("");
+		System.out.println();
 		
-		B = solution(A, 2);
+		try {
+			B = solution(A, 2);
+		} catch(ArrayIndexOutOfBoundsException e) {
+			B = A;
+		}
 		
 		System.out.print("After : ");
 		for(int i : B)
@@ -19,16 +24,15 @@ public class Codility_02 {
     public static int[] solution(int[] A, int K) {
     	int N = A.length;
     	int tmp;
-    	if(N <= 0) {
-    	}
-    	else { 
+//    	try {
 	        for(int i = 0; i<K; ++i) {
 	             tmp = A[N-1];
 	            for(int j=N-1; j>0; --j)
 	                A[j] = A[j-1];
 	            A[0] = tmp;
 	        }
-    	}
+//    	} catch (ArrayIndexOutOfBoundsException e) {
+//    	} 
         return A;
     }
 }
