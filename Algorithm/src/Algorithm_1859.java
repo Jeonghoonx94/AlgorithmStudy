233b0ba71bf84e9e1ffc42fcdb8fd0dfd23279fe
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-03-28 13:05:36 +0900

    SWEA 1859 - Millionaire Project (D2)

---
+++ Algorithm/src/Algorithm_1859.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Algorithm_1859 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int ans = 0;
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0; i<n; ++i)
+            	arr[i] = sc.nextInt();
+            int max = arr[n-1];
+            for(int i= n-2; i>=0; --i) {
+            	if(max<arr[i]) max = arr[i];
+            	else ans += max-arr[i];
+            }
+            System.out.println("#"+test_case+" "+ans);
+		}
+		sc.close();
+	}
+
+}
