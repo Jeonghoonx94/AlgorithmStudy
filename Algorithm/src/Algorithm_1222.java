bb07f685c67f88332a643857c34bf5ed1f7275c3
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-04-01 15:40:34 +0900

    SWEA 1222 - 계산기1 (D4)

---
+++ Algorithm/src/Algorithm_1222.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Algorithm_1222 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		for(int test_case = 1; test_case <= 1; test_case++) {
+			int result = 0;
+			int length = sc.nextInt();
+			String s = sc.next();
+			
+			result = solve(s, length);
+			System.out.println("#"+test_case+" "+result);
+		}
+		sc.close();
+	}
+	
+	static int solve(String s, int length) {
+		int result = 0;
+		for(int i=0; i<length; ++i) {
+			if(s.charAt(i)>='0' && s.charAt(i)<='9') {
+				result += s.charAt(i)-'0';
+			}
+		}
+		return result;
+	}
+}
