7d7f717038010d1e6d8d27f56f73575a2b915d97
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-04-03 16:37:59 +0900

    SWEA 1233 - 사칙연산 유효성 검사 (D4)

---
+++ Algorithm/src/Algorithm_1233.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Algorithm_1233 {
+	static char[] tree;	// 트리
+	static int n;	// 트리의 노드 수
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = 10;
+		for(int test_case = 1; test_case<=T; ++test_case) {
+
+			n = sc.nextInt();	// 트리의 노드 수
+			tree = new char[n+1];	// 노드 번호의 시작은 1부터
+			for(int i=0; i<n; ++i)
+				tree[sc.nextInt()] = sc.nextLine().charAt(1);	// 공백부터 읽어지므로 1
+			
+			System.out.println("#"+test_case+" "+inorder_traverse(n));
+		}
+		sc.close();
+	}
+	
+	static int inorder_traverse(int i) {	// i: 트리의 마지막 노드 위치
+		int result = 1;
+		for(int a = i; a > 0; --a) {
+			char c = tree[a];
+			if(a>i/2) {	// 단말노드
+				if(c >= '0' && c <='9')
+					result = 1;
+				else {
+					result = 0;
+					break;
+				}
+			} else {	// 부모노드
+				if(c == '+' || c=='-' || c=='*' || c=='/')	// 연산자면
+					result = 1;
+				else {
+					result = 0;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+}
