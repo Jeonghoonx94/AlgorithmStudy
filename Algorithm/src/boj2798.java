0a363e4856069fa2c451487e7c3b0ff482f83647
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-04-08 14:51:15 +0900

    BOJ 2798 - 블랙잭

--- Algorithm/src/boj2798.java
+++ Algorithm/src/boj2798.java
@@ -23,31 +23,16 @@ public class boj2798 {
 		st = new StringTokenizer(br.readLine());	// = new StringTokenizer(br.readLine(), " "); 
 		br.close();
 		int[] a = new int[n];	// n개의 숫자를 넣기 위한 배열
-		int[] ans = new int[n*(n-1)*(n-2)/6];	// n개에서 3개의 조합(중복x) : nC_3
-		int count = -1;	// 
+		int max = Integer.MIN_VALUE;
 		for(int i=0; i<n; ++i)
 //			a[i] = Integer.parseInt(arr[i]);
 			a[i] = Integer.parseInt(st.nextToken());
-		for(int i=0; i<n-2; ++i) {
-			for(int j=i+1; j<n-1; ++j) {
-				for(int k = j+1; k<n; ++k) {
-					ans[++count] = a[i]+a[j]+a[k];
-					if(ans[count] == m) break;	// m과 같은 수가 있다면 반복문 종료
-				}
-				if(ans[count] == m) break;
-			}
-			if(ans[count] == m) break;
-		}
-		if(ans[count] == m)
-			bw.write(ans[count]+"\n");
-		else {
-			Arrays.sort(ans);	// 오름차순으로 정렬
-			for(int i = ans.length-1; i>=0; --i)
-				if(ans[i] < m) {
-					bw.write(ans[i]+"\n");
-					break;
-				}
-		}
+		for(int i=0; i<n-2; ++i)
+			for(int j=i+1; j<n-1; ++j)
+				for(int k = j+1; k<n; ++k)
+					if(max<a[i]+a[j]+a[k] && m>=a[i]+a[j]+a[k])
+						max = a[i]+a[j]+a[k];
+		bw.write(max+"\n");
 		bw.flush();
 		bw.close();
 	}
