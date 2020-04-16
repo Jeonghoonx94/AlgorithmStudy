13f55a88b8013787cdea2850051f7222d7d3d21d
Jeonghoonx94 <wjdgnsx94@naver.com>
2020-04-16 15:30:48 +0900

    BOJ 1003 - The number of 0&1 in Fibo Seq, BOJ 2748 fix

--- Algorithm/src/boj2748.java
+++ Algorithm/src/boj2748.java
@@ -9,15 +9,11 @@ public class boj2748 {
 	}
 	static long fibo(int n)
 	{
-		long[] fibo = new long[100];
+		long[] fibo = new long[n+1];
 		fibo[0] = 0;
-		if(n <1)
-			return fibo[n];
-        else {
-        	fibo[1] = 1;
-		    for(int i=2; i<=n; ++i)
-			    fibo[i] = fibo[i-1] + fibo[i-2];
-        }
+    	fibo[1] = 1;
+	    for(int i=2; i<=n; ++i)
+		    fibo[i] = fibo[i-1] + fibo[i-2];
 		return fibo[n];
 	}
 }
